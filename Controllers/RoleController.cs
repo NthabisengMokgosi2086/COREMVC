c51343509d808426877e36f95b7d6ec94c77962e
NthabisengMokgosi2086 <Nthabisengmokgosi51@gmail.com>
2020-11-17 05:00:32 +0200

    Role-Security-Access
    Roles to access a certain controller after login-in

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COREMVC.Controllers
+{
+    [Authorize]
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+
+        /// 
+        /// Injecting Role Manager
+        /// 
+        /// 
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [Authorize(Roles = "Human Resource,Manager")]
+        public IActionResult Index()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+    }
+}
